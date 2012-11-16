5764ef1007571816a6ee4be32a7e278d9fa42c9f
Julien Finet <julien.finet@kitware.com>
2012-11-16 11:24:02 -0500

    ENH: Add support for external projects passed as configuration parameter

--- SuperBuild.cmake
+++ SuperBuild.cmake
@@ -137,6 +137,10 @@ if(Slicer_BUILD_SlicerWebGLExport)
   list(APPEND Slicer_DEPENDENCIES SlicerWebGLExport)
 endif()
 
+if(DEFINED Slicer_ADDITIONAL_DEPENDENCIES)
+  list(APPEND Slicer_DEPENDENCIES ${Slicer_ADDITIONAL_DEPENDENCIES})
+endif()
+
 SlicerMacroCheckExternalProjectDependency(Slicer)
 
 #-----------------------------------------------------------------------------
@@ -184,6 +188,8 @@ set(ep_cmake_boolean_args
   Slicer_BUILD_MultiVolumeExplorer
   Slicer_BUILD_MultiVolumeImporter
   Slicer_BUILD_SlicerWebGLExport
+  Slicer_BUILD_Extensions
+  Slicer_BUILD_SkullStripper
   )
 
 # Add the option to disable Java even when found on the machine.
@@ -294,6 +300,26 @@ if(Slicer_BUILD_EXTENSIONMANAGER_SUPPORT)
   list(APPEND ep_superbuild_extra_args
     -DqMidasAPI_DIR:PATH=${qMidasAPI_DIR}
     )
+endif()
+
+# Projects that Slicer needs to download/configure/build...
+if(Slicer_ADDITIONAL_DEPENDENCIES)
+  foreach(additional_dependency ${Slicer_ADDITIONAL_DEPENDENCIES})
+    list(APPEND ep_superbuild_extra_args
+      -D${additional_dependency}_DIR:PATH=${${additional_dependency}_DIR})
+  endforeach()
+endif()
+
+# Projects that Slicer needs to include
+if(Slicer_ADDITIONAL_PROJECTS)
+  # needed by packaging
+  list(APPEND ep_superbuild_extra_args
+    -DSlicer_ADDITIONAL_PROJECTS:STRING=${Slicer_ADDITIONAL_PROJECTS})
+  # needed to do find_package within Slicer
+  foreach(additional_project ${Slicer_ADDITIONAL_PROJECTS})
+    list(APPEND ep_superbuild_extra_args
+      -D${additional_project}_DIR:PATH=${${additional_project}_DIR})
+  endforeach()
 endif()
 
 # Set CMake OSX variable to pass down the external project
